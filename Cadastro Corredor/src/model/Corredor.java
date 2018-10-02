6080093243defbe0c02505dc1a445ed5222025d3
FranciscoMelloJr <franciscomelloxpe@gmail.com>
2018-10-01 21:14:08 -0300

    att

--- Cadastro Corredor/src/model/Corredor.java
+++ Cadastro Corredor/src/model/Corredor.java
@@ -1,6 +1,5 @@
 package model;
 
-
 import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleDoubleProperty;
@@ -10,84 +9,83 @@ import javafx.beans.property.StringProperty;
 
 public class Corredor {
 
-	private StringProperty  nome  = new SimpleStringProperty("");
-	private StringProperty  nasc  = new SimpleStringProperty("");
-	private DoubleProperty  peito  = new SimpleDoubleProperty();
+	private StringProperty nome = new SimpleStringProperty("");
+	private StringProperty nasc = new SimpleStringProperty("");
+	private DoubleProperty peito = new SimpleDoubleProperty();
 	private IntegerProperty idade = new SimpleIntegerProperty();
 	private IntegerProperty distancia = new SimpleIntegerProperty();
-	private SimpleStringProperty faixaEtaria = new SimpleStringProperty();
-	
-	
+	private SimpleStringProperty faixa = new SimpleStringProperty();
+
 	public final StringProperty nomeProperty() {
 		return this.nome;
 	}
-	
+
 	public final String getNome() {
 		return this.nomeProperty().get();
 	}
-	
+
 	public final void setNome(final String nome) {
 		this.nomeProperty().set(nome);
 	}
-	
-	public final StringProperty nascProperty() {
-		return this.nasc;
-	}
-	
-	public final String getNasc() {
-		return this.nascProperty().get();
-	}
-	
-	public final void setNasc(final String nasc) {
-		this.nascProperty().set(nasc);
-	}
-	
+
 	public final DoubleProperty peitoProperty() {
 		return this.peito;
 	}
-	
+
 	public final double getPeito() {
 		return this.peitoProperty().get();
 	}
-	
+
 	public final void setPeito(final double peito) {
 		this.peitoProperty().set(peito);
 	}
-	
+
 	public final IntegerProperty idadeProperty() {
 		return this.idade;
 	}
-	
+
 	public final int getIdade() {
 		return this.idadeProperty().get();
 	}
-	
+
 	public final void setIdade(final int idade) {
 		this.idadeProperty().set(idade);
 	}
-	
+
 	public final IntegerProperty distanciaProperty() {
 		return this.distancia;
 	}
-	
+
 	public final int getDistancia() {
 		return this.distanciaProperty().get();
 	}
-	
+
 	public final void setDistancia(final int distancia) {
 		this.distanciaProperty().set(distancia);
 	}
-	
-	public final SimpleStringProperty faixaEtariaProperty() {
-		return this.faixaEtaria;
+
+	public final SimpleStringProperty faixaProperty() {
+		return this.faixa;
 	}
-	
-	public final String getFaixaEtaria() {
-		return this.faixaEtariaProperty().get();
+
+	public final String getFaixa() {
+		return this.faixaProperty().get();
 	}
-	
-	public final void setFaixaEtaria(final String faixaEtaria) {
-		this.faixaEtariaProperty().set(faixaEtaria);
-	}	
-	
+
+	public final void setFaixa(final String faixa) {
+		this.faixaProperty().set(faixa);
+	}
+
+	public final StringProperty nascProperty() {
+		return this.nasc;
+	}
+
+	public final String getNasc() {
+		return this.nascProperty().get();
+	}
+
+	public final void setNasc(final String nasc) {
+		this.nascProperty().set(nasc);
+	}
+
 }
--- Cadastro Corredor/src/view/PrincipalController.java
+++ Cadastro Corredor/src/view/PrincipalController.java
@@ -1,5 +1,70 @@
 package view;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import model.Corredor;
+
 public class PrincipalController {
-	
+
+	@FXML
+	TextField txtNome;
+	@FXML
+	TextField txtPeito;
+	@FXML
+	TextField txtDistancia;
+	@FXML
+	DatePicker txtDataNascimento;
+
+	@FXML
+	TableView<Corredor> tbl;
+	@FXML
+	TableColumn<Corredor, String> colNome;
+	@FXML
+	TableColumn<Corredor, Number> colPeito;
+	@FXML
+	TableColumn<Corredor, Number> colDistancia;
+	@FXML
+	TableColumn<Corredor, String> colFaixa;
+
+	private ArrayList<Corredor> corredores = new ArrayList<>();
+
+	@FXML
+	public void initialize() {
+		inicializaTbl();
+	}
+
+	private void inicializaTbl() {
+		colNome.setCellValueFactory(cellData -> cellData.getValue().nomeProperty());
+		colPeito.setCellValueFactory(cellData -> cellData.getValue().peitoProperty());
+		colDistancia.setCellValueFactory(cellData -> cellData.getValue().distanciaProperty());
+		colFaixa.setCellValueFactory(cellData -> cellData.getValue().faixaProperty());
+
+	}
+
+	private int calculaIdadeJava8(LocalDate dtNasc) {
+		LocalDate hoje = LocalDate.now();
+		long idade = ChronoUnit.YEARS.between(dtNasc, hoje);
+		return (int) idade;
+	}
+
+	@FXML
+	public void incluir() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		Corredor c = new Corredor();
+		c.setNome(txtNome.getText());
+		c.setNasc(dtf.format(txtDataNascimento.getValue()));
+		c.setIdade(calculaIdadeJava8(txtDataNascimento.getValue()));
+		corredores.add(c);
+		tbl.setItems(FXCollections.observableArrayList(corredores));
+	}
+
 }
