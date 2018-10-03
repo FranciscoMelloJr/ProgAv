f7d05283732b2b89c985bd16f5db131e43e38f01
FranciscoMelloJr <franciscomelloxpe@gmail.com>
2018-10-02 23:42:09 -0300

    att

--- Cadastro Competicao/src/view/PrincipalController.java
+++ Cadastro Competicao/src/view/PrincipalController.java
@@ -1,5 +1,6 @@
 package view;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
@@ -9,7 +10,6 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-
 import model.Competicao;
 
 public class PrincipalController {
@@ -62,18 +62,63 @@ public class PrincipalController {
 		c.setDistancia(Integer.parseInt(txtDistancia.getText()));
 		competicoes.add(c);
 		tbl.setItems(FXCollections.observableArrayList(competicoes));
-		limpaTela();
+		melhor();
+		// limpaTela();
 
 	}
 
 	@FXML
-	public void remover() {
+	public void selecionar() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 		Competicao c = tbl.getSelectionModel().getSelectedItem();
-		for (Competicao x : competicoes)
-			if (c.toString().equals(x.toString()))
-				tbl.getItems().removeAll(tbl.getSelectionModel().getSelectedItems());
-		System.out.println(competicoes.toString());
+
+		txtNome.setText(c.getNome());
+		txtColocacao.setText(String.format("%d", c.getColocação()));
+		txtDistancia.setText(String.format("%s", c.getDistancia()));
+		txtData.setValue(LocalDate.parse(c.getData(), dtf));
+
+	}
+
+	public void remover() {
+
+		tbl.setOnMouseClicked(e -> {
+			if (e.getClickCount() > 1) {
+				selecionar();
+
+				competicoes.remove(tbl.getSelectionModel().getSelectedItem());
+				tbl.setItems(FXCollections.observableArrayList(competicoes));
+				melhor();
+			}
+		});
+
+		// Competicao c = tbl.getSelectionModel().getSelectedItem();
+		// for (Competicao x : competicoes)
+		// if (c.toString().equals(x.toString())) {
+		// tbl.getItems().removeAll(tbl.getSelectionModel().getSelectedItems());
+		// competicoes.remove(x);
+		// tbl.getSelectionModel().clearSelection();
+		// }
+		// System.out.println("chamou o metodo melhor");
+		// tbl.setItems(FXCollections.observableArrayList(competicoes));
+		// melhor();
+	}
+
+	@FXML
+	public void melhor() {
+		int melhor = competicoes.get(0).getColocação();
+		System.out.println("Entrou no melhor " + melhor);
+		for (Competicao c : competicoes) {
+			System.out.println(c.getColocação() + " NO FOR  " + melhor);
+			if (c.getColocação() < melhor) {
+				System.out.println(c.getColocação() + " NO IF  " + melhor);
+				melhor = c.getColocação();
+			}
+		}
+		System.out.println("Setou o melhor " + melhor);
+
+		txtMelhor.setText(Integer.toString(melhor));
+
 	}
 
 	@FXML
