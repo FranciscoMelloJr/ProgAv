d8060e55ec21db34f030a9f4b7db6a9da8389da1
FranciscoMelloJr <franciscomelloxpe@gmail.com>
2018-10-17 12:37:40 -0300

    att

--- Projeto/src/view/Main.java
+++ Projeto/src/view/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Tela.fxml"));
-			Scene scene = new Scene(root);
+			Scene scene = new Scene(root,1080,720);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
