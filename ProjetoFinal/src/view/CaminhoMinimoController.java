ec42c141724a4094e065b3a90846fa72577fc70d
FranciscoMelloJr <franciscomelloxpe@gmail.com>
2018-10-27 19:31:26 -0300

    att

--- ProjetoFinal/src/view/CaminhoMinimoController.java
+++ ProjetoFinal/src/view/CaminhoMinimoController.java
@@ -1,9 +1,16 @@
 package view;
 
+import java.io.FileReader;
+import java.util.Properties;
+
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import model.Aresta;
+import model.Fila;
 import model.Vertice;
 
 public class CaminhoMinimoController {
@@ -12,6 +19,9 @@ public class CaminhoMinimoController {
 	TextField txtSource;
 	@FXML
 	TextField txtDestiny;
+	
+	@FXML
+	RadioButton ckDestiny;
 
 	@FXML
 	TableView<Vertice> tbl;
@@ -21,5 +31,127 @@ public class CaminhoMinimoController {
 	TableColumn<Vertice, Number> colDistancia;
 	@FXML
 	TableColumn<Vertice, String> colPath;
+	
+	
+	Fila fila = new Fila();
+	Vertice destiny;
+
+	@FXML
+	public void initialize() {
+		inicializaTbl();
+	}
+
+	private void lerArquivoProperties() {
+		Properties propertie = new Properties();
+		try (FileReader fr = new FileReader("conf.properties")) {
+			propertie.load(fr);
+			txtInstituicaoEnsino.setText(propertie.getProperty("nome"));
+			txtMediaA.setText(propertie.getProperty("mediaA"));
+			txtMediaR.setText(propertie.getProperty("mediaR"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	public void finalizar() {
+
+		while (fila.tamanho() != 0) {
+			Vertice atual = fila.remove();
+			for (int i = 0; i < atual.getAdj().size(); i++) {
+				if (!atual.getAdj().get(i).isPerm()) {
+					for (Aresta aresta : arestaLista) {
+						if (ckOrientado.isSelected()) {
+							if ((atual.getNome().equals(aresta.getOrigem()))
+									&& (atual.getAdj().get(i).getNome().equals(aresta.getDestino()))) {
+								alteraDist(atual, aresta, i);
+							}
+						} else {
+							if ((atual.getNome().equals(aresta.getOrigem())
+									|| atual.getNome().equals(aresta.getDestino()))
+									&& (atual.getAdj().get(i).getNome().equals(aresta.getDestino())
+											|| atual.getAdj().get(i).getNome().equals(aresta.getOrigem()))) {
+								alteraDist(atual, aresta, i);
+							}
+						}
+					}
+				}
+			}
+			insereADJ(atual);
+			atual.setPerm(true);
+			if ((ckDestiny.isSelected()) && (destiny.isPerm())) {
+				break;
+			}
+		}
+		tbl.setItems(FXCollections.observableArrayList(verticeLista));
+	}
+
+	public void alteraDist(Vertice atual, Aresta aresta, int i) {
+
+		for (Vertice vertice : verticeLista) {
+			if (vertice.getNome().equals(atual.getAdj().get(i).getNome())) {
+				if (atual.getDistancia() + aresta.getValor() < vertice.getDistancia()) {
+					vertice.setDistancia((atual.getDistancia() + aresta.getValor()));
+					vertice.setPath(atual.getNome());
+				}
+			}
+		}
+	}
+
+	public void insereADJ(Vertice vertice) {
+
+		for (int i = 0; i < vertice.getAdj().size(); i++) {
+			if (!vertice.getAdj().get(i).isPerm()) {
+				if (!(fila.verificaIgual((vertice.getAdj().get(i).getNome())))) {
+					fila.insere(vertice.getAdj().get(i));
+				}
+			}
+		}
+	}
+
+	@FXML
+	public void adicionaDestiny() {
+
+		for (Vertice vertice : verticeLista) {
+			if (vertice.getNome().equals(txtDestiny.getText())) {
+				destiny = vertice;
+			}
+		}
+		txtDestiny.setText("");
+
+	}
+
+	@FXML
+	public void destinySN() {
+		if (ckDestiny.isSelected()) {
+			txtDestiny.setDisable(false);
+		} else {
+			txtDestiny.setDisable(true);
+		}
+	}
+
+	@FXML
+	public void adicionaSource() {
+
+		Vertice source = null;
+		for (Vertice vertice : verticeLista) {
+			if (vertice.getNome().equals(txtSource.getText())) {
+				source = vertice;
+			}
+		}
+		source.setDistancia(0);
+		source.setPerm(true);
+		fila.insere(source);
+		txtSource.setText("");
+
+	}
+
+
+	private void inicializaTbl() {
+		colNome.setCellValueFactory(cellData -> cellData.getValue().nomeProperty());
+		colDistancia.setCellValueFactory(cellData -> cellData.getValue().distanciaProperty());
+		colPath.setCellValueFactory(cellData -> cellData.getValue().pathProperty());
+
+	}
 	
 }
--- ProjetoFinal/src/view/PrincipalController.java
+++ ProjetoFinal/src/view/PrincipalController.java
@@ -1,5 +1,12 @@
 package view;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.Properties;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -7,9 +14,11 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
+import model.Aresta;
+import model.Vertice;
 
 public class PrincipalController {
-	
+
 	@FXML
 	TabPane tabPane;
 	@FXML
@@ -25,27 +34,154 @@ public class PrincipalController {
 	TextField txtDestino;
 	@FXML
 	TextField txtValor;
-	
+
+	ArrayList<Vertice> verticeLista = new ArrayList<Vertice>();
+	ArrayList<Aresta> arestaLista = new ArrayList<Aresta>();
+
+	private void alteraArquivoProperties() {
+		Properties propertie = new Properties();
+		try (FileReader fr = new FileReader("conf.properties"); BufferedReader br = new BufferedReader(fr)) {
+			propertie.load(fr);
+			propertie.setProperty("orientado", String.valueOf(ckOrientado.isSelected()));
+			propertie.setProperty("valorado", String.valueOf(ckOrientado.isSelected()));
+
+			try (BufferedWriter bw = new BufferedWriter(new FileWriter("conf.properties"))) {
+				propertie.store(bw, "Atualização de Configurações");
+				bw.close();
+
+			} catch (Exception ex) {
+				System.out.println(ex.getMessage());
+				ex.printStackTrace();
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void inserirAresta() {
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("aresta.txt", true))) {
+
+			Aresta aresta = new Aresta();
+			aresta.setOrigem(txtOrigem.getText());
+			aresta.setDestino(txtDestino.getText());
+			if (ckValorado.isSelected())
+				aresta.setValor(Integer.parseInt(txtValor.getText()));
+			arestaLista.add(aresta);
+			adicionaAdjacente(aresta);
+			limpaTelaE();
+			txtOrigem.requestFocus();
+
+			String origem = String.format("%-5.5s", aresta.getOrigem());
+			String destino = String.format("%-5.5s", aresta.getDestino());
+			String valor = String.format("%03d", aresta.getValor()).substring(0, 3);
+
+			bufferedWriter.append(origem + destino + valor + "\n");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void inserirVertice() {
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("vertice.txt", true))) {
+
+			Vertice vertice = new Vertice();
+			vertice.setNome(txtVertice.getText());
+			verticeLista.add(vertice);
+			txtVertice.setText("");
+
+			String nome = String.format("%-5.5s", vertice.getNome());
+
+			bufferedWriter.append(nome + "\n");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void adicionaAdjacente(Aresta aresta) {
+
+		for (Vertice vertice : verticeLista) {
+			if (vertice.getNome().equals(aresta.getOrigem())) {
+				for (Vertice adjacente : verticeLista) {
+					if (adjacente.getNome().equals(aresta.getDestino())) {
+						vertice.getAdj().add(adjacente);
+						adjacente.getAdj().add(vertice);
+					}
+				}
+			}
+		}
+	}
+
+	@FXML
+	public void adicionaAresta() {
+
+		Aresta aresta = new Aresta();
+		aresta.setOrigem(txtOrigem.getText());
+		aresta.setDestino(txtDestino.getText());
+		if (ckValorado.isSelected())
+			aresta.setValor(Integer.parseInt(txtValor.getText()));
+		arestaLista.add(aresta);
+		adicionaAdjacente(aresta);
+		limpaTelaE();
+		txtOrigem.requestFocus();
+	}
+
+	@FXML
+	public void adicionaVertice() {
+
+		Vertice vertice = new Vertice();
+		vertice.setNome(txtVertice.getText());
+		verticeLista.add(vertice);
+		txtVertice.setText("");
+
+	}
+
+	@FXML
+	public void limpaTelaE() {
+
+		txtOrigem.setText("");
+		txtValor.setText("");
+		txtDestino.setText("");
+
+	}
+
+	@FXML
+	public void focusValor() {
+		txtValor.requestFocus();
+	}
+
+	@FXML
+	public void focusDestino() {
+		txtDestino.requestFocus();
+
+	}
+
 	@FXML
 	public void abreAdjacenciaIncidencia() {
+		alteraArquivoProperties() ;
 		abreTab("Matriz/Lista", "ListaMatriz.fxml");
 	}
-	
+
 	@FXML
 	public void abreDistancia() {
+		alteraArquivoProperties();
 		abreTab("Matriz", "Distancia.fxml");
 	}
-	
+
 	@FXML
 	public void abreCaminhoMinimo() {
+		alteraArquivoProperties();
 		abreTab("Caminho Minímo", "CaminhoMinimo.fxml");
 	}
-	
+
 	@FXML
 	public void abre() {
 
 	}
-	
+
 	private void abreTab(String titulo, String path) {
 		try {
 			Tab tab = tabAberta(titulo);
@@ -73,5 +209,5 @@ public class PrincipalController {
 	private void selecionaTab(Tab tab) {
 		tabPane.getSelectionModel().select(tab);
 	}
-	
+
 }
