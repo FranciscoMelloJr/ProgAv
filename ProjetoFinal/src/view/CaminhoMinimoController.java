b4416c733e82e212d5f35e8f3fc64b494d1fb3a1
FranciscoMelloJr <franciscomelloxpe@gmail.com>
2018-10-27 17:58:27 -0300

    att

--- ProjetoFinal/src/view/CaminhoMinimoController.java
+++ ProjetoFinal/src/view/CaminhoMinimoController.java
@@ -1,5 +1,25 @@
 package view;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import model.Vertice;
+
 public class CaminhoMinimoController {
 
+	@FXML
+	TextField txtSource;
+	@FXML
+	TextField txtDestiny;
+
+	@FXML
+	TableView<Vertice> tbl;
+	@FXML
+	TableColumn<Vertice, String> colNome;
+	@FXML
+	TableColumn<Vertice, Number> colDistancia;
+	@FXML
+	TableColumn<Vertice, String> colPath;
+	
 }
